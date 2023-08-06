9c1ff685dfbee41307c8a8adb314b81e2acd818d
Tayef <tayefhossain200@gmail.com>
2023-08-06 09:58:20 -0800

    full code added

--- script.js
+++ script.js
@@ -3,12 +3,97 @@ const scroll = new LocomotiveScroll({
   smooth: true,
 });
 
-function circleMouseFollower() {
+let timeOut;
+
+function cricleStyle() {
+  let xscale = 1;
+  let yscale = 1;
+
+  let xprev = 0;
+  let yprev = 0;
+
+  window.addEventListener("mousemove", function (dets) {
+    clearTimeout(timeOut);
+
+    xscale = gsap.utils.clamp(0.8, 1.2, dets.clientX - xprev);
+    yscale = gsap.utils.clamp(0.8, 1.2, dets.clientY - yprev);
+
+    xprev = dets.clientX;
+    yprev = dets.clientY;
+    circleMouseFollower(xscale, yscale);
+
+    timeOut = setTimeout(function () {
+      document.querySelector(
+        "#mincircle"
+      ).style.transform = `translate(${dets.clientX}px, ${dets.clientY}px) scale(1, 1)`;
+    }, 100);
+  });
+}
+
+function firstPageAnim() {
+  let tl = gsap.timeline();
+
+  tl.from("#nav", {
+    y: "-10",
+    opacity: 0,
+    duration: 1.5,
+    ease: Expo.easeInOut,
+  })
+    .to(".boundingelem", {
+      y: 0,
+      ease: Expo.easeInOut,
+      duration: 2,
+      delay: -1,
+      stagger: 0.2,
+    })
+
+    .from("#herofooter", {
+      y: -10,
+      opacity: 0,
+      duration: 1.5,
+      delay: -1,
+      ease: Expo.easeInOut,
+    });
+}
+
+function circleMouseFollower(xscale, yscale) {
   window.addEventListener("mousemove", function (dets) {
     document.querySelector(
       "#mincircle"
-    ).style.transform = `translate(${dets.clientX}px, ${dets.clientY}px)`;
+    ).style.transform = `translate(${dets.clientX}px, ${dets.clientY}px) scale(${xscale}, ${yscale})`;
   });
 }
 
 circleMouseFollower();
+
+firstPageAnim();
+
+cricleStyle();
+
+document.querySelectorAll(".elem ").forEach(function (elem) {
+  let rotate = 0;
+  let diffrot = 0;
+
+  elem.addEventListener("mouseleave", function (dets) {
+    gsap.to(elem.querySelector("img"), {
+      opacity: 0,
+      ease: Power3,
+      duration: 0.5,
+    });
+  });
+
+  elem.addEventListener("mousemove", function (dets) {
+    let diff = dets.clientY - elem.getBoundingClientRect().top;
+
+    diffrot = dets.clientX - rotate;
+    rotate = dets.clientX;
+
+    gsap.to(elem.querySelector("img"), {
+      opacity: 1,
+      ease: Power3,
+      top: diff,
+      left: dets.clientX,
+      rotate: gsap.utils.clamp(-20, 20, diffrot * 0.5),
+    });
+  });
+});
